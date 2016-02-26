92e90a3beb70079a878627ca4dca7bd8703e7c89
Maxwell Hadley <mch6706@rit.edu>
2016-02-26 12:09:12 -0500

    Create SensorTag.py created initiliazation for connection to sensor tag

---
+++ python/SensorTag.py
@@ -0,0 +1,27 @@
+import pexpect
+import time
+
+class SensorTag:
+
+    __init__(self, mac_address):
+        self.gatt_term = pexpect.spawn("sh run_gatt.sh " + mac_address)
+        self.gatt_term.expect("\[*\]\[LE\]> ")
+        connection_attempts = 0
+        while connection_attempts < 12:
+            print("Attempting Sensor Tag connection #" + str(connection_attempts + 1))
+            self.gatt_term.sendline("connect")
+            response = self.gatt_term.expect( \
+            ["Error: connect error: Transport endpoint is not connected*","Connection Successful","*Error*"])
+            if response == 1:
+                print("Could not connect to device at Sensor Tag at mac address: " + mac_address + "; make sure the device is on.")
+                print("Trying to connect again in 5 seconds.")
+                time.sleep(5)
+            elif response == 2:
+                print("Sucessfully connected to sensor tag at mac address: " + mac_address)
+                #self.gatt_term.expect("\[*\]\[LE\]> ")
+                break
+            elif response == 3:
+                pass
+            connection_attempts += 1
+        return response - 2
+            
