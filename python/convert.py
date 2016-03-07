686e9a0ec2cae9ecea0764ddd48d0a36d889ce6d
David Thong Nguyen <thnguyen2808@gmail.com>
2016-03-07 10:22:00 -0500

    Added Temp and and Movement calculations. NEED REVISION

--- python/convert.py
+++ python/convert.py
@@ -1,5 +1,52 @@
 import math
 
+# Constant for Temp Sensor
+SCALE_LSB = 0.03125
+
+# Constants (acceleration range) for Movement Sensor
+ACC_RANGE_2G = 0
+ACC_RANGE_4G = 1
+ACC_RANGE_8G = 2
+ACC_RANGE_16G = 3
+
+# PASTED FROM USER GUIDE
+"""
+void sensorTmp007Convert(uint16_t rawAmbTemp, uint16_t rawObjTemp, float *tAmb, float *tObj)
+{
+  const float SCALE_LSB = 0.03125;
+  float t;
+  int it;
+
+  it = (int)((rawObjTemp) >> 2);
+  t = ((float)(it)) * SCALE_LSB;
+  *tObj = t;
+
+  it = (int)((rawAmbTemp) >> 2);
+  t = (float)it;
+  *tTgt = t * SCALE_LSB;
+}
+"""
+def temp(raw):
+    # if obj temp
+    # if amb temp
+    it = int(raw >> 2)
+    temp = float(it) * SCALE_LSB
+    return temp
+
+# Seems in the user guide, acc_range is defined somewhere in this file,
+# not necessarily a parameter
+def mvmt(raw, acc_range):
+    val = None
+    if acc_range == ACC_RANGE_2G:
+        val = float(raw) / (32768/2)
+    elif acc_range == ACC_RANGE_4G:
+        val = float(raw) / (32768/4)
+    elif acc_range == ACC_RANGE_8G:
+        val = float(raw) / (32768/8)
+    elif acc_range == ACC_RANGE_16G:
+        val = float(raw) / (32768/16)
+    return val
+
 def humid_temp(raw):
     temp = raw[2:] + raw[0:2]
     temp = int(temp, 16)
