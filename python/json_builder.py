552a0e79f96b13466e85c87b7a57c0151a4e0b55
David Thong Nguyen <thnguyen2808@gmail.com>
2016-03-11 11:05:06 -0500

    Pretty printing JSON file made from Python

--- python/json_builder.py
+++ python/json_builder.py
@@ -6,19 +6,17 @@ def main():
     con = sqlite3.connect('sensor_data.sqlite')
     cur = con.cursor()
     cur.execute("SELECT id, time, temp, pressure FROM BARO")
+
     data_array = []
     for row in cur:
         dicts = collections.OrderedDict()
-        print(str(row[0]))
-        print(str(row[1]))
-        print(str(row[2]))
         dicts['id'] = str(row[0])
         dicts['time'] = str(row[1])
         dicts['temp'] = str(row[2])
         dicts['pressure'] = str(row[3])
         data_array.append(dicts)
     con.close()
-    j = json.dumps(data_array)
+    j = json.dumps(data_array, indent=4, separators=(',', ': '))
     data_file = '../www/html/json/current.json'
     f = open(data_file, 'w')
     f.write(j)
