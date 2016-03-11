abc5f37e6c39bb2fd9af29d198787701b2216984
Max <mch6706@rit.edu>
2016-03-11 15:58:43 -0500

    Added basic get functions for tables and then get_hourly for irtemp

--- python/dao.py
+++ python/dao.py
@@ -1,14 +1,15 @@
 import sqlite3
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 
+TIME_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
 DB_NAME = 'sensor_data.sqlite'
 
 class DAO:
-  
+
     connection = None
     cursor = None
     saving = False
-  
+
     def __init__(self, file_name, saving):
         self.connection = sqlite3.connect(DB_NAME)
         self.cursor = self.connection.cursor()
@@ -45,11 +46,47 @@ class DAO:
         self.save()
 
     def get_irtemp(self,entries=1):
-        data = self.cursor.execute("SELECT * FROM IRTEMP AS ir WHERE ir.time > ? ORDER BY ir.id DESC LIMIT ?",
+        data = self.cursor.execute("SELECT * FROM IRTEMP AS ir WHERE ir.time > ? ORDER BY ir.time DESC LIMIT ?",
                                    (datetime.today().replace(hour=0,minute=0,second=0,microsecond=0),str(entries))).fetchall()
         return data
+
     def get_hourly_irtemp(self):
-        pass
+        last_day = datetime.today().replace(minute=0,second=0,microsecond=0) - timedelta(days=1)
+        data = self.cursor.execute("SELECT * FROM IRTEMP AS ir WHERE ir.time > ? ORDER BY ir.time ASC",
+                                   (last_day,)).fetchall()
+
+        hourly_data = []
+        i = 0
+
+        for hour in range(0,24):
+            while(i < len(data)):
+                data_el = datetime.strptime(data[i][1],TIME_FORMAT)
+                if data_el.hour == ((hour + last_day.hour) % 24):
+                    hourly_data.append(data[i])
+                    i += 1
+                    break
+                elif data_el.hour > ((hour + last_day.hour) % 24):
+                    break
+                i += 1
+
+        return hourly_data
 
     def get_daily_irtemp(self):
         pass
+
+    def get_humid(self,entries=1):
+        data = self.cursor.execute("SELECT * FROM HUMID AS h WHERE h.time > ? ORDER BY h.id DESC LIMIT ?",
+                                   (datetime.today().replace(hour=0,minute=0,second=0,microsecond=0),str(entries))).fetchall()
+        return data
+
+    def get_baro(self,entries=1):
+        data = self.cursor.execute("SELECT * FROM BARO AS b WHERE b.time > ? ORDER BY b.id DESC LIMIT ?",
+                                   (datetime.today().replace(hour=0,minute=0,second=0,microsecond=0),str(entries))).fetchall()
+        return data
+
+    def get_opti(self,entries=1):
+        data = self.cursor.execute("SELECT * FROM OPTI AS o WHERE o.time > ? ORDER BY o.id DESC LIMIT ?",
+                                   (datetime.today().replace(hour=0,minute=0,second=0,microsecond=0),str(entries))).fetchall()
+        return data
+
+
