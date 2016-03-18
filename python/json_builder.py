40c72b95d9aafa7481d818772ebe78adf8fd15af
Brian Nugent <bnugent@mail.rit.edu>
2016-03-18 10:42:06 -0400

    Working on getting jsonbuilder to support history, need to take a look at how the json is being formed for history, and how to order it so that we can list out multiple times

--- python/json_builder.py
+++ python/json_builder.py
@@ -11,8 +11,8 @@ class JSONBuilder:
     def __init__(self, db):
         self.db = db
         
-    def buildBaro(self):
-        baro = self.db.get_baro()
+    def buildBaro(self, buildType):
+        baro = self.db.get_recent("baro")
         dicts = collections.OrderedDict()
         dicts['id'] = str(baro[0][0])
         dicts['time'] = str(baro[0][1])
@@ -20,8 +20,8 @@ class JSONBuilder:
         dicts['pressure'] = str(baro[0][4])
         self.data_wrapper["baro"] = dicts
 
-    def buildHumid(self):
-        humid = self.db.get_humid()
+    def buildHumid(self, buildType):
+        humid = self.db.get_recent("humid")
         dicts = collections.OrderedDict()
         dicts['id'] = str(humid[0][0])
         dicts['time'] = str(humid[0][1])
@@ -29,8 +29,8 @@ class JSONBuilder:
         dicts['humidity'] = str(humid[0][4])
         self.data_wrapper["humid"] = dicts
 
-    def buildIRTemp(self):
-        irtemp = self.db.get_irtemp()
+    def buildIRTemp(self, buildType):
+        irtemp = self.db.get_recent("irtemp")
         dicts = collections.OrderedDict()
         dicts['id'] = str(irtemp[0][0])
         dicts['time'] = str(irtemp[0][1])
@@ -38,26 +38,36 @@ class JSONBuilder:
         dicts['ambtemp'] = str(irtemp[0][4])
         self.data_wrapper["irtemp"] = dicts
 
-    def buildOpti(self):
-        opti = self.db.get_opti()
+    def buildOpti(self, buildType):
         dicts = collections.OrderedDict()
-        dicts['id'] = str(opti[0][0])
-        dicts['time'] = str(opti[0][1])
-        dicts['light'] = str(opti[0][3])
+        if(buildType == "current"):
+            opti = self.db.get_recent("opti")
+            dicts['id'] = str(opti[0][0])
+            dicts['time'] = str(opti[0][1])
+            dicts['light'] = str(opti[0][3])
+        else:
+            opti = self.db.get_hourly("opti")
+            print(opti)
+            for index in range(0, len(opti)):
+                dicts['id'] = str(opti[index][0])
+                dicts['time'] = str(opti[index][1])
+                dicts['light'] = str(opti[index][3])
         self.data_wrapper["opti"] = dicts
 
-    def buildFile(self):
-        self.buildBaro()
-        self.buildHumid()
-        self.buildIRTemp()
-        self.buildOpti()
+    def buildFile(self, buildType):
+        data_file = '../www/html/json/' + buildType + '.json'
+        #self.buildBaro(buildType)
+        #self.buildHumid(buildType)
+        #self.buildIRTemp(buildType)
+        self.db.open(False)
+        self.buildOpti(buildType)
         j = json.dumps(self.data_wrapper, indent=4, separators=(',', ': '))
-        data_file = '../www/html/json/current.json'
         f = open(data_file, 'w')
         f.write(j)
         print(j)
-
+        self.db.close()
+            
 
 #for debugging
-jsonB = JSONBuilder(DAO("sensor_data.sqlite",False))
-jsonB.buildFile()
+jsonB = JSONBuilder(DAO("sensor_data.sqlite"))
+jsonB.buildFile("history")
