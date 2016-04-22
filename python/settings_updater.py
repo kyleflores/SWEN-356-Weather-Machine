cefa0e1f907a3e831eb1e1f77b65f837ac58e6e3
Brian Nugent <bnugent@mail.rit.edu>
2016-04-22 10:34:11 -0400

    Was calling the wrong method name, fixed it.

--- python/settings_updater.py
+++ python/settings_updater.py
@@ -4,7 +4,7 @@ class SettingsUpdater:
 
     def __init__(self):
         data = json.load(sys.stdin)
-        buildFile(data)
+        updateFile(data)
 
     def updateFile(self, data):
         settingsFile = '../www/html/json/settings.json'
